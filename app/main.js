10f571e7e80f0bbca0516dd457083651b4c04777
waqasahmad34 <48016736+waqasahmad34@users.noreply.github.com>
2021-06-07 18:34:07 +0500

    add prod link

--- app/main.js
+++ app/main.js
@@ -26,7 +26,7 @@ app.use(helmet());
 // Enable CORS with various options
 // https://github.com/expressjs/cors
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://main.dkwi04pdt4wvv.amplifyapp.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   next();
